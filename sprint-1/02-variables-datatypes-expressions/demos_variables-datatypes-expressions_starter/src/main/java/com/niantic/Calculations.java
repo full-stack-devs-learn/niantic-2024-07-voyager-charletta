5168eb4689a90edd890bc33413ce46c78eeeda83
Charletta Harris <icharharris@gmail.com>
2024-07-23 14:53:08 -0700

    follow code demo

--- sprint-1/02-variables-datatypes-expressions/demos_variables-datatypes-expressions_starter/src/main/java/com/niantic/Calculations.java
+++ sprint-1/02-variables-datatypes-expressions/demos_variables-datatypes-expressions_starter/src/main/java/com/niantic/Calculations.java
@@ -4,6 +4,12 @@ public class Calculations
 {
     public static void main(String[] args)
     {
+         // Calculate how many donuts per student
+        int numberOfStudents =4;
+        int numberOfDonuts =22;
+        double donutsPerStudent = (double)numberOfDonuts / numberOfStudents;
+
+
 
     }
 }
