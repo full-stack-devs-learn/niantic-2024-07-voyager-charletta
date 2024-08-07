b54b4e2e9068485012cb8de97c5da631bff712c7
Charletta Harris <icharharris@gmail.com>
2024-08-06 21:12:01 -0700

    exercise

--- sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_2_objects/MathTest.java
+++ sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_2_objects/MathTest.java
@@ -1,5 +1,53 @@
-package com.niantic.part_2_objects;
 
 public class MathTest {
+    private int score;
+    private final int possiblePoints;
+    private final String studentName;
 
+
+    public MathTest(int possiblePoints, String studentName) {
+        this.possiblePoints = possiblePoints;
+        this.studentName = studentName;
+
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getPossiblePoints() {
+        return possiblePoints;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public String getLetterGrade() {
+        int percent = getPercent();
+
+        if (percent >= 90) {
+            return "A";
+        }else
+        if (percent >= 80) {
+            return "B";
+        }else
+        if (percent >= 70) {
+            return "C";
+        }else
+        if (percent >= 60) {
+            return "D";
+        }
+        return "F";
+    }
+
+    public int getPercent() {
+        return (score * 100) / possiblePoints;
+    }
 }
+
+
