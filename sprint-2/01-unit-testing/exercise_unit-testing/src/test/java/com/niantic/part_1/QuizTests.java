7b3ad8d307a3d78136f37ab70b996406ee68ab69
Charletta Harris <icharharris@gmail.com>
2024-08-21 08:30:11 -0700

    assignment

--- sprint-2/01-unit-testing/exercise_unit-testing/src/test/java/com/niantic/part_1/QuizTests.java
+++ sprint-2/01-unit-testing/exercise_unit-testing/src/test/java/com/niantic/part_1/QuizTests.java
@@ -2,28 +2,71 @@ package com.niantic.part_1;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class QuizTests
 {
     @Test
-    public void testGetPercentage()
+    public void test_GetScore()
+
+    { // Arrange
+        Quiz quiz = new Quiz (100,"Silk Rose");
+        int expected=65;
+        quiz.setScore(expected);
+
+        // Act
+        int actualScore = quiz.getScore();
+
+        // Assert
+        assertEquals(expected,actualScore, "The score that runs should match the get score and the set score");
+
+    }
+
+
+        @Test
+        public void test_GetPossiblePoints()
     {
-       //arrange
-       // studentScore= new Student Score();
+        // Arrange
+        Quiz quiz = new Quiz(100,"Silk Rose");
+        int expected=100;
 
+        // Act
+        int actual = quiz.getPossiblePoints();
 
-
-
+        // Assert
+        assertEquals(expected, actual,("This test should return the exact Possible Points you can get on Test"));
     }
-       //act
+        @Test
+        public void test_StudentName()
+        {@Test
+            public void testGetStudentName() {
+            // Arrange
+            Quiz quiz = new Quiz(100,"Silk Rose");
+            int expected=100;
 
+            // Act
+            String actual= quiz.getStudentName();
 
-       // assert
-
-
-
-
+            // Assert
+            assertEquals(expected, actual, "This should return the actual student Names");
     }
+
+
+
+
+        }
+
+
+
+
+
+
 
 
 
 }
+
+
+
+
+
