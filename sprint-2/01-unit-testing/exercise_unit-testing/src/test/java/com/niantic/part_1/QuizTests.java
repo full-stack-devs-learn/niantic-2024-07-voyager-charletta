8e0c1b6a669d645d8dfdd62d02c918a8f957b2ef
Charletta Harris <icharharris@gmail.com>
2024-08-21 22:56:18 -0700

    updated

--- sprint-2/01-unit-testing/exercise_unit-testing/src/test/java/com/niantic/part_1/QuizTests.java
+++ sprint-2/01-unit-testing/exercise_unit-testing/src/test/java/com/niantic/part_1/QuizTests.java
@@ -22,51 +22,106 @@ class QuizTests
 
     }
 
-
-        @Test
-        public void test_GetPossiblePoints()
+    @Test
+    public void test_GetPossiblePoints()
     {
         // Arrange
         Quiz quiz = new Quiz(100,"Silk Rose");
         int expected=100;
 
         // Act
-        int actual = quiz.getPossiblePoints();
+
+        int actual=quiz.getPossiblePoints();
 
         // Assert
         assertEquals(expected, actual,("This test should return the exact Possible Points you can get on Test"));
     }
-        @Test
-        public void test_StudentName()
-        {@Test
-            public void testGetStudentName() {
-            // Arrange
-            Quiz quiz = new Quiz(100,"Silk Rose");
-            int expected=100;
 
-            // Act
-            String actual= quiz.getStudentName();
+    @Test
+    public void test_GetPercentage() {
+        // Arrange
+        Quiz quiz = new Quiz(100, "Silk Rose");
+        quiz.setScore(100);
+        int expectedPercentage = 100;
 
-            // Assert
-            assertEquals(expected, actual, "This should return the actual student Names");
+        // Act
+        int actualPercentage = quiz.getPercent();
+
+        // Assert
+        assertEquals(expectedPercentage,actualPercentage,"This test should make calculate the percentage");
     }
 
 
+    @Test
+    public void test_getLetterGradeA() {
+        // arrange
+        Quiz quiz = new Quiz(100, "Silk Rose");
+        quiz.setScore(90);
+        String expectedGrade = "A";
 
+        //act
+        String actualGrade = quiz.getLetterGrade();
 
-        }
+        //assert
+        assertEquals(expectedGrade, actualGrade, " The letter grade received should be A");
+    }
+    @Test
+    public void testGetLetterGrade_B() {
+        // Arrange
+        Quiz quiz = new Quiz(100, "Silk Rose");
+        quiz.setScore(85);
+        String expectedGrade = "B";
 
+        // Act
+        String actualGrade = quiz.getLetterGrade();
 
+        // Assert
+        assertEquals(expectedGrade, actualGrade,"The letter grade received should be B");
+    }
 
+    @Test
+    public void testGetLetterGrade_C() {
+        // Arrange
+        Quiz quiz = new Quiz(100, "Silk Rose");
+        quiz.setScore(75);
+        String expectedGrade = "C";
 
+        // Act
+        String actualGrade = quiz.getLetterGrade();
 
+        // Assert
+        assertEquals(expectedGrade, actualGrade, "The letter grade received should be C");
+    }
 
+    @Test
+    public void testGetLetterGrade_D() {
+        // Arrange
+        Quiz quiz = new Quiz(100, "Silk Rose");
+        quiz.setScore(65);
+        String expectedGrade = "D";
 
+        // Act
+        String actualGrade = quiz.getLetterGrade();
+
+        // Assert
+        assertEquals(expectedGrade, actualGrade, "The letter grade received should be D");
+    }
+
+    @Test
+    public void testGetLetterGrade_F() {
+        // Arrange
+        Quiz quiz = new Quiz(100, "Silk Rose");
+        quiz.setScore(50);
+        String expectedGrade = "F";
+
+        // Act
+        String actualGrade = quiz.getLetterGrade();
+
+        // Assert
+        assertEquals(expectedGrade, actualGrade, "The letter grade received should be F");
+    }
+
+    
 
 
 }
-
-
-
-
-
--- sprint-2/02-inheritance/exercise_inheritance/src/main/java/com/niantic/Wizard.java
+++ sprint-2/02-inheritance/exercise_inheritance/src/main/java/com/niantic/Wizard.java
@@ -1,5 +1,87 @@
 package com.niantic;
 
-public class Wizard
+public class Wizard extends Character
 {
+    private int mana;
+
+    public Wizard(String name, int health, int level, int experience, int mana)
+    {
+
+        super(name, health, level, experience);
+
+        this.mana = mana;
+    }
+
+    public int getMana() {
+        return this.mana;
+    }
+
+    public void addMana() {
+        if (!isDefeated()) {
+            this.mana++;
+            System.out.println(this.getName() + " added mana and now has " + this.mana + " mana.");
+        } else {
+            System.out.println(this.getName() + " cannot add mana because they have been defeated.");
+        }
+    }
+
+
+    public void castSpell(Character target) {
+
+        if (getHealth() <= 0) {
+            System.out.println("Cannot cast super attack. The wizard has been defeated.");
+        }
+        if (mana >= 10) {
+            mana -= 10;
+            int superAttackDamage = 2 * (attackDamage);
+
+            System.out.println("Super attack cast! Dealt " + superAttackDamage + " damage.");
+            target.takeDamage(superAttackDamage);
+
+        } else {
+            System.out.println("Not enough mana to cast super attack.");
+        }
+    }
+
+    public void regenerateMana(int amount) {
+        if (this.getHealth() <= 0) {
+            System.out.println(this.getName() + " cannot regenerate mana because they have been defeated.");
+            return;
+        }
+        this.mana += amount;
+        System.out.println(this.getName() + " regenerated " + amount + " mana and now has " + this.mana + " mana.");
+
+
+    }
+
+
+    @Override
+    public void levelUp() {
+        if (!isDefeated()) {
+            super.levelUp();
+            this.mana += 10; System.out.println(this.getName() + " leveled up and gained 10 mana. Current mana: " + this.mana);
+        } else {
+            System.out.println(this.getName() + " cannot level up because they have been defeated.");
+        }
+    }
+
+    @Override
+    public String specialAbility() {
+        return "Cast Spell";
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
