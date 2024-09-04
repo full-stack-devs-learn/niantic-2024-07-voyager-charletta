9b9a5ac4ab46c621ab04fc22d19623b960753e11
Charletta Harris <icharharris@gmail.com>
2024-09-04 11:53:08 -0700

    exercise

--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
@@ -6,21 +6,53 @@
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function displayListTitle() {}
 
+
+function displayListTitle()
+{         const pageTitle = document.getElementById("syilk's shopping list");
+          pageTitle.textContent = "Page Title"
+    }
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayGroceries() 
+{    const service = new ShoppingService();
+     const ShoppingList=service.getShoppingList()
+                         .sort((left,right) => left.isComplete-right.isComplete)
+                         .map(grocery => {
+                              return{
+                              ...grocery,
+                              statusText: grocery.isComplete ? "complete" : "incomplete"
+                        };
+        });
+
+    const groceryListContainer = document.getElementById("groceries"); 
+
+    ShoppingList.forEach(grocery => {
+        createGroceryDiv(grocery, groceryListContainer);
+    });
+
+
+}
+
 
 /**
  * This function will be called when the button is clicked. You will need to get a reference
  * to every list item and add the class completed to each one
  */
-function markCompleted() {
+function markCompleted()
+{
+    
+        const divs = document.querySelectorAll(".groceries.grocery-item");
+    
+        divs.forEach(div => div.ShoppingList.add("completed"));
+
+
 }
 
+document.addEventListener("DOMContentLoaded",() =>{
 
 displayListTitle();
 displayGroceries();
-
+});
+    
--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-service.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-service.js
@@ -1,22 +1,91 @@
 class ShoppingService {
 
-    getListName() {
+    getListName() 
+    { return 'Syilks Grocery List';
+         
     }
 
     getShoppingList() {
-        return [
+        return [ 
             {
                 id: 1,
-                title: '5 lb sack of potatoes',
+                title: ' frozen chicken sausage',
                 quantity: 1,
                 isComplete: false
             },
             {
                 id: 2,
-                title: 'Frozen Pizza',
+                title: 'teriyaki seaweed',
                 quantity: 4,
                 isComplete: true
+
+            },
+
+            {   
+                id:3,
+                title:'dino chicken nuggets', 
+                quantity: 3,
+                isComplete:false
+
+            },
+    
+            {   
+                id:4,
+                title:'blueberry pancakes', 
+                quantity: 2,
+                isComplete:true
+
+            },
+
+            {   
+                id:5,
+                title:'bananas', 
+                quantity: 4,
+                isComplete:false
+
+            },
+
+            {   
+                id:6,
+                title:'oranges', 
+                quantity: 2,
+                isComplete:true
+
+            },
+
+            {   
+                id:7,
+                title:'chocolate milk', 
+                quantity: 1,
+                isComplete:false
+
+            },
+
+            {   
+                id:8,
+                title:'1lb broccoli', 
+                quantity: 1,
+                isComplete:true
+
+            },
+
+            {   
+                id:9,
+                title:'string beans', 
+                quantity: 2,
+                isComplete:false
+
+            },
+
+            {   
+                id:10,
+                title:'lollipop', 
+                quantity: 2,
+                isComplete:true
+
             }
-        ];
+        ]
     }
+
+    
 }
