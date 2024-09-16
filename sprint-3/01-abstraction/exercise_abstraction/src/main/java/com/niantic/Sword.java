bf316ef22fda9de449021130ea7420fa0bb51450
Charletta Harris <icharharris@gmail.com>
2024-09-16 13:07:49 -0700

    finished prompt need unit testing

--- sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Sword.java
+++ sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Sword.java
@@ -1,21 +1,46 @@
 package com.niantic;
 
-public class Sword extends Weapon
+public abstract class Sword extends Weapon
 {
-    private String Name;
-    private int Damage;
-    private int daggerCount;
+    private String name;
+    private int damage;
 
     public Sword(String name, int damage)
     {
         super(name, damage);
+        this.name = name;
+        this.damage = damage;
+    }
 
+    @Override
+    public int attack()
+    {
+        int currentCharge = getPercentCharged();
+        setPercentCharged(Math.min(100, currentCharge + 10));
+        return getDamage();
+    }
 
+    @Override
+    public int powerAttack()
+    {
+        int currentCharge=getPercentCharged();
 
+        if (currentCharge < 50) {
+            return attack();
+        } else if (currentCharge <= 90) {
+            setPercentCharged(currentCharge - 50);
+            return damage * 2;
+        } else if (currentCharge == 100) {
+            setPercentCharged(0);
+            return damage * 4;
+        }
+
+        return powerAttack();
 
     }
 
-
+    @Override
+    public int getRange() {return 1;}
 
 }
 
---
+++ sprint-3/01-abstraction/exercise_abstraction/src/test/java/com/niantic/SwordTest.java
@@ -0,0 +1,36 @@
+package com.niantic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Modifier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SwordTest extends ReflectionBase<Sword>
+{
+    Class<Sword> klass;
+
+    @BeforeEach
+    void setup()
+    {
+        klass = Sword.class;
+    }
+
+    @Test
+    public void testAttack()
+
+
+{
+    // arrange
+
+
+    // act/assert
+
+}
+
+
+
+
+}
+
