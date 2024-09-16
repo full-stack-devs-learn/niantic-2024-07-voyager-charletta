ecc4d4157a80ae8eec90d2b31c582fe1d1b0d62a
Charletta Harris <icharharris@gmail.com>
2024-09-16 08:30:15 -0700

    working on assignment

--- sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Sword.java
+++ sprint-3/01-abstraction/exercise_abstraction/src/main/java/com/niantic/Sword.java
@@ -1,5 +1,21 @@
 package com.niantic;
 
-public class Sword
+public class Sword extends Weapon
 {
+    private String Name;
+    private int Damage;
+    private int daggerCount;
+
+    public Sword(String name, int damage)
+    {
+        super(name, damage);
+
+
+
+
+    }
+
+
+
 }
+
